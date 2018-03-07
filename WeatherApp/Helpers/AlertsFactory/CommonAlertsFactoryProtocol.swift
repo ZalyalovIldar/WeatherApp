2d616865ab9ecea50196489f1932a4fb2d2744a0
Timbaev <hometim55@gmail.com>
2018-03-07 11:22:39 +0300

    Prepared project for start

---
+++ WeatherApp/Helpers/AlertsFactory/CommonAlertsFactoryProtocol.swift
@@ -0,0 +1,15 @@
+//
+//  CommonAlertsFactoryProtocol.swift
+//  WeatherApp
+//
+//  Created by Тимур Шафигуллин on 07.03.18.
+//  Copyright © 2018 iOSLab. All rights reserved.
+//
+
+import UIKit
+
+protocol CommonAlertsFactoryProtocol: class {
+    
+    func getErrorAlert(with message: String) -> UIViewController
+    
+}
